f3cb57b0432d3f0c06d883a95ef35114353cfbeb
Alija Hodzic <hodzic.alija5@gmail.com>
2022-02-19 15:06:17 +0100

    added edit, delete and single birthday retriever

--- server/src/controllers/birthday.controller.js
+++ server/src/controllers/birthday.controller.js
@@ -1,14 +1,11 @@
-const moment = require("moment");
 const { Birthday } = require("../models/birthday.model");
 
 exports.createBirthday = async (req, res, next) => {
   const { name, birthday, email, phone } = req.body;
 
-  const formatedBirthday = birthday;
-
   await Birthday.create({
     name,
-    birthday: formatedBirthday,
+    birthday,
     email,
     phone,
     photo: req.file.filename,
@@ -16,8 +13,8 @@ exports.createBirthday = async (req, res, next) => {
     // keys required by calendar, it's how it makes events
 
     title: name,
-    start: formatedBirthday,
-    end: formatedBirthday,
+    start: birthday,
+    end: birthday,
   });
 
   // Return all birthdays with last created always first in array
@@ -27,6 +24,47 @@ exports.createBirthday = async (req, res, next) => {
 };
 
 exports.getBirthdays = async (req, res, next) => {
+  let allBirthdays = await Birthday.find().sort({ createdAt: "desc" });
+
+  res.status(200).json(allBirthdays);
+};
+
+exports.getSingleBirthday = async (req, res, next) => {
+  const { id } = req.params;
+
+  let singleBirthday = await Birthday.findOne({ _id: id });
+
+  res.status(200).json(singleBirthday);
+};
+
+exports.deleteBirthday = async (req, res, next) => {
+  const { id } = req.params;
+
+  await Birthday.findByIdAndDelete(id);
+
+  let allBirthdays = await Birthday.find().sort({ createdAt: "desc" });
+
+  res.status(200).json(allBirthdays);
+};
+
+exports.editBirthday = async (req, res, next) => {
+  const { name, birthday, email, phone, img } = req.body;
+  const { id } = req.params;
+
+  let birthdayForEdit = await Birthday.findOne({ _id: id });
+
+  birthdayForEdit.name = name;
+  birthdayForEdit.birthday = birthday;
+  birthdayForEdit.email = email;
+  birthdayForEdit.phone = phone;
+  // depending if photo has been changed
+  birthdayForEdit.photo = req.file ? req.file.filename : img;
+  birthdayForEdit.title = name;
+  birthdayForEdit.start = birthday;
+  birthdayForEdit.end = birthday;
+
+  await birthdayForEdit.save();
+
   let allBirthdays = await Birthday.find().sort({ createdAt: "desc" });
 
   res.status(200).json(allBirthdays);
