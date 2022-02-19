a600ea8ee041ffc63506a9fce8b950b98cf3274f
Alija Hodzic <hodzic.alija5@gmail.com>
2022-02-19 15:02:35 +0100

    added default image and message for when no image is fetched

--- client/src/components/modal/MyModal.js
+++ client/src/components/modal/MyModal.js
@@ -4,6 +4,8 @@ import moment from "moment";
 
 import "./modal.css";
 import { getPhoto } from "./getPhoto";
+import Loader from "../utils/Loader";
+import ifDateIsTooOld from "../../assets/images/noimage.png";
 
 function MyModal({ selectedBirthday, setSelectedBirthday }) {
   const [photoOfTheDay, setPhotoOfTheDay] = useState(false);
@@ -17,12 +19,11 @@ function MyModal({ selectedBirthday, setSelectedBirthday }) {
     selectedBirthday && getPhoto(setPhotoOfTheDay, selectedBirthday.birthday, setIsLoading);
   }, [selectedBirthday]);
 
-  console.log(photoOfTheDay);
   return (
     <div>
-      <Modal open={selectedBirthday} onClose={handleCloseModal}>
+      <Modal open={!!selectedBirthday} onClose={handleCloseModal}>
         {isLoading ? (
-          <Typography>Loading..</Typography>
+          <Loader />
         ) : (
           <div className="modal-content">
             <img className="avatar" id="modal-avatar" src={`http://localhost:5000/uploads/${selectedBirthday.photo}`} />
@@ -33,10 +34,16 @@ function MyModal({ selectedBirthday, setSelectedBirthday }) {
               {selectedBirthday.phone} | {selectedBirthday.email}
             </Typography>
 
-            <img className="photo-of-the-day" src={photoOfTheDay.hdurl} />
+            <img className="photo-of-the-day" src={!photoOfTheDay ? ifDateIsTooOld : photoOfTheDay?.hdurl} />
 
             <div className="description">
-              <Typography variant="body2">{photoOfTheDay.explanation}</Typography>
+              {!photoOfTheDay && (
+                <Typography variant="body2">
+                  NASA started publishing photos on June 16, 1995. We are sorry if you are older than this date.
+                </Typography>
+              )}
+
+              <Typography variant="body2">{photoOfTheDay?.explanation}</Typography>
             </div>
           </div>
         )}
