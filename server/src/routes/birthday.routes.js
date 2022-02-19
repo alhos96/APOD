41366a019454190d1a5408cf2a41d978c59ea894
Alija Hodzic <hodzic.alija5@gmail.com>
2022-02-19 15:05:22 +0100

    added edit and delete routes

--- server/src/routes/birthday.routes.js
+++ server/src/routes/birthday.routes.js
@@ -3,9 +3,13 @@ const fileUpload = require("../middlewares/multer");
 
 const router = express.Router();
 
-const { createBirthday, getBirthdays } = require("../controllers/birthday.controller");
+const { createBirthday, getBirthdays, getSingleBirthday, editBirthday, deleteBirthday } = require("../controllers/birthday.controller");
 
 router.post("/form", fileUpload.single("img"), createBirthday);
 router.get("/calendar", getBirthdays);
+router.get("/edit/:id", getSingleBirthday);
+router.patch("/edit/:id", fileUpload.single("img"), editBirthday);
+
+router.delete("/:id", deleteBirthday);
 
 module.exports = router;
