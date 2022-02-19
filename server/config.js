1e6f5ad4cc6708e8886bd5754a9f7c91a7f6eeea
Alija Hodzic <hodzic.alija5@gmail.com>
2022-02-19 15:08:24 +0100

    aded secret word for sending token when admin logs in

--- server/config.js
+++ server/config.js
@@ -3,6 +3,7 @@ require("dotenv").config();
 const config = {
   port: process.env.PORT || 5000,
   mongo: process.env.MONGO_URI || `mongodb://localhost:27017`,
+  secret: process.env.SECRET,
 };
 
 module.exports = config;
